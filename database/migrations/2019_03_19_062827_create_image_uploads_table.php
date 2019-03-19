f09aa62962371a020352f4a5308f099be8b8acb7
thusitha_wijethunga <thusithawijethunga@gmail.com>
2019-03-19 12:14:44 +0530

    init code
    init code

---
+++ database/migrations/2019_03_19_062827_create_image_uploads_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImageUploadsTable extends Migration
+{
+    //create_image_uploads_table
+
+	public function up()
+    {
+        Schema::create('image_uploads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('filename');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_uploads');
+    }
+}
